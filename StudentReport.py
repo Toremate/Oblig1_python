d41b6fbdee7ced8d7b147cb6b94312b8d4134439
Tore <tbergseth@gmail.com>
2026-01-31 21:20:51 +0100

    #Done

--- StudentReport.py
+++ StudentReport.py
@@ -56,25 +56,53 @@ def calculate_grade(students):
 def write_results(file_path_output, graded):
     from pathlib import Path
     from datetime import datetime
-
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    filename =f"Students Graded {timestamp}.txt"
-    full_path = Path(file_path_output) / filename
-    with open(f"{full_path}", "w") as f:
-        for student_id, name, mark, grade in graded:
-            f.write(f"{student_id}, {name}, {mark}, {grade}\n")
+    try:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        filename =f"Students Graded {timestamp}.txt"
+        full_path = Path(file_path_output) / filename
+        with open(full_path, "w") as f:
+            for student_id, name, mark, grade in graded:
+                f.write(f"{student_id}, {name}, {mark}, {grade}\n")
+        return True
+    except Exception as e:
+        return False
 
 # Task5 - Report Generator
-def generate_report(graded):
-    file_path = input("Enter file path")
-    file_path_output = input("Enter path to save new file")
+def generate_report(file_path, file_path_output, graded):
+
 
     total_students = len(graded)
-    average_marks = 0
-    numbers_of_A = 0
-    numbers_of_B = 0
-    numbers_of_C = 0
-    numbers_of_D = 0
+    sum_marks = 0
+    counts = {"A":0, "B":0, "C":0, "D":0}
+    for _, _, mark, grade in graded:
+        sum_marks += mark
+        counts[grade] += 1
 
-    for student_id in graded:
-        average_marks += average_marks
+    average_mark = sum_marks / total_students if total_students else 0
+
+    print(f"""
+    Input file path: {file_path}
+    Output file path: {file_path_output}
+        *** Students grades report ***
+        Total Students: {total_students}
+        Average Mark: {average_mark}
+        Number of A grades: {counts["A"]}
+        Number of B grades: {counts["B"]}
+        Number of C grades: {counts["C"]}
+        Number of D grades: {counts["D"]}""")
+
+def main():
+    file_path = input("Enter absolute file path: ")
+    file_path_output = input("Enter absolute output file path: ")
+    lines = read_students(file_path)
+    students = process_students(lines)
+    graded = calculate_grade(students)
+    write_results_bool = write_results(file_path_output, graded)
+    if write_results_bool:
+        generate_report(file_path, file_path_output, graded)
+        print("The output file is written successfully.")
+    else:
+        print("Something went wrong.")
+
+if __name__=="__main__":
+    main()
