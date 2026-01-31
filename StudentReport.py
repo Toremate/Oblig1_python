deef2df551ca8045493b90d6d2d22a480ffe9840
Tore <tbergseth@gmail.com>
2026-01-31 14:58:17 +0100

    Task 4

--- StudentReport.py
+++ StudentReport.py
@@ -1,3 +1,5 @@
+
+
 # Task1 - File reader
 def read_students(file_path):
     lines = []
@@ -24,12 +26,55 @@ def process_students(lines):
 
     for line in lines:
         try:
-            name, mark = line.split(",")
-            students.append((name, int(mark)))
+            student_id, name, mark = line.split(",")
+            students.append((student_id, name, int(mark)))
         except ValueError:
             print(f"Invalid record: {line}")
 
     return students
 
 # Task3 - Grade calculator
-# def calculate_grade(marks):
+def calculate_grade(students):
+    graded = []
+    for student_id, name, mark in students:
+
+        if mark > 89:
+            grade = "A"
+
+        elif mark > 79  and mark < 90:
+            grade = "B"
+
+        elif mark > 59 and mark < 80:
+            grade = "C"
+
+        else: grade = "D"
+
+        graded.append((student_id, name, mark, grade))
+    return graded
+
+# Task4 - Write Output File
+def write_results(file_path_output, graded):
+    from pathlib import Path
+    from datetime import datetime
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    filename =f"Students Graded {timestamp}.txt"
+    full_path = Path(file_path_output) / filename
+    with open(f"{full_path}", "w") as f:
+        for student_id, name, mark, grade in graded:
+            f.write(f"{student_id}, {name}, {mark}, {grade}\n")
+
+# Task5 - Report Generator
+def generate_report(graded):
+    file_path = input("Enter file path")
+    file_path_output = input("Enter path to save new file")
+
+    total_students = len(graded)
+    average_marks = 0
+    numbers_of_A = 0
+    numbers_of_B = 0
+    numbers_of_C = 0
+    numbers_of_D = 0
+
+    for student_id in graded:
+        average_marks += average_marks
